0de36573a6fad57f8e50df2b2c0fb0110e193b8b
Nguyen Hoang Phuc <phucnguyenhoang3839@gmail.com>
2024-09-04 15:40:34 +0700

    PHUC | fix error v1

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,8 +1,8 @@
-const { createServer } = "node:http";
-const { routing } = requestuire("./routing");
+const { createServer } = require("node:http");
+const { routing } = require("./router");
 const hostname = "127.0.0.1";
 const port = 3000;
-
+require;
 const server = createServer((request, response) => {
   routing(request, response);
 });
